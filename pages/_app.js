b7fca752a08fe0e089b9a591f532bdac46281345
hsglc <huseyingulcuu@gmail.com>
2021-10-28 13:08:50 +0300

    feat: Design the root page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
+/** @format */
+
+import "../styles/globals.css";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="antialiased text-gray-700">
+      <Header />
+      <main className="my-6">
+        <Component {...pageProps} />
+      </main>{" "}
+      <footer className="w-full bg-green-400 font-medium py-2">
+        <p className="site-container">makes with passion by huseyin gulcu</p>
+      </footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
